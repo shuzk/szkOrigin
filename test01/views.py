2cf90045d3c357ccc0fd59738a7c380eb7e432fd
szkOrigin <szkOrigin@163.com>
2018-11-23 19:32:25 +0800

    11路由反解析

--- test01/views.py
+++ test01/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 def index(request):
-    return HttpResponse('hello')
+    a = reverse('test01:index')
+    return HttpResponse('hello %s '%a)
