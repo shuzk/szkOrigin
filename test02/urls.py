a9cc7a5f0b5094ea529ada2247c921c174008c43
szkOrigin <szkOrigin@163.com>
2018-11-28 11:46:09 +0800

    25——模板使用2

--- test02/urls.py
+++ test02/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     # url(r'^student/', views.StudentView)
-    url(r'^index/', views.index)
+    url(r'^index/', views.index),
+    url(r'^index2/', views.index2),
 ]
--- test02/views.py
+++ test02/views.py
@@ -18,7 +18,7 @@
 #         else:
 #             return render(request, 'student.html', {'form': form})
 
-
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -27,6 +27,18 @@ def index(request):
     # 1.获取模板
     tem = loader.get_template('index.html')
     # 2.渲染模板
-    content = { 'city': '北京' }
+    content = {'city': '北京'}
     return HttpResponse(tem.render(content))
-    return reder(request, 'index.html', content)
+    return render(request, 'index.html', context)
+
+
+def index2(request):
+    context = {
+        'city': '北京',
+        'adict': {
+            'name': '西游记',
+            'author': '吴承恩'
+        },
+        'alist': [1, 2, 3, 4, 5]
+    }
+    return render(request, 'index.html', context)
