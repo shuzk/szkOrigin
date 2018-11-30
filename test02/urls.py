577e9be36b86bc8f07564658b3fca4a78f1ccbff
szkOrigin <szkOrigin@163.com>
2018-11-30 15:03:52 +0800

    31——url视图集添加url不能加/

--- test02/urls.py
+++ test02/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 ]
 
 router = DefaultRouter()  # 可以处理视图的路由器
-router.register(r'^studentsViewSet/', views.StudentsInfoViewSet)  # 向路由器中注册视图集
+# 不能加^和/
+router.register(r'studentsViewSet', views.StudentsInfoViewSet)  # 向路由器中注册视图集
 urlpatterns += router.urls  # 将路由器中的所有路由信息追到django的路由列表中
