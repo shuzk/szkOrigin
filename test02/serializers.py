180b89271865a347fd369674fb45967885f40bd4
szkOrigin <szkOrigin@163.com>
2018-11-30 15:59:58 +0800

    32——序列化基本使用

--- test02/serializers.py
+++ test02/serializers.py
@@ -5,7 +5,12 @@ from test02.models import StudentsInfo
 
 class StudentsInfoSerializer(serializers.ModelSerializer):
     """学生表数据序列器"""
-
+    sid = serializers.IntegerField(label='ID', read_only=True)
+    sname = serializers.CharField(label='名字', max_length=20)
+    sage = serializers.IntegerField(label='年龄', required=False)
+    sgender = serializers.BooleanField(label='性别', required=False)
+    is_delete = serializers.BooleanField(label='逻辑删除', required=False)
+    image = serializers.ImageField(label='图片', required=False)
     class Meta:
         model = StudentsInfo
         fields = '__all__'
